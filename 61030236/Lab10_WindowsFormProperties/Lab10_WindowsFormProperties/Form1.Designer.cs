a8d87cd90fa98cccdf8cac0d513344913ff7d52f
Toeyty <61030236@kmitl.ac.th>
2019-11-17 12:03:48 +0700

    commit

--- 61030236/Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.Designer.cs
+++ 61030236/Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Hello World";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- 61030236/Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.cs
+++ 61030236/Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.cs
@@ -16,5 +16,10 @@ namespace Lab10_WindowsFormProperties
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
