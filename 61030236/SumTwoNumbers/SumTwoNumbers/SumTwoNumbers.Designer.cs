4d1956488200502d574d9789f84695d987dc1510
Toeyty <61030236@kmitl.ac.th>
2019-11-17 13:39:26 +0700

    commit

---
+++ 61030236/SumTwoNumbers/SumTwoNumbers/SumTwoNumbers.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SumTwoNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNumberLabel = new System.Windows.Forms.Label();
+            this.secondNumberLabel = new System.Windows.Forms.Label();
+            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.sumLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNumberLabel
+            // 
+            this.firstNumberLabel.AutoSize = true;
+            this.firstNumberLabel.Location = new System.Drawing.Point(183, 59);
+            this.firstNumberLabel.Name = "firstNumberLabel";
+            this.firstNumberLabel.Size = new System.Drawing.Size(100, 20);
+            this.firstNumberLabel.TabIndex = 0;
+            this.firstNumberLabel.Text = "First Number";
+            // 
+            // secondNumberLabel
+            // 
+            this.secondNumberLabel.AutoSize = true;
+            this.secondNumberLabel.Location = new System.Drawing.Point(159, 112);
+            this.secondNumberLabel.Name = "secondNumberLabel";
+            this.secondNumberLabel.Size = new System.Drawing.Size(124, 20);
+            this.secondNumberLabel.TabIndex = 1;
+            this.secondNumberLabel.Text = "Second Number";
+            this.secondNumberLabel.Click += new System.EventHandler(this.secondNumberLabel_Click);
+            // 
+            // firstNumberTextBox
+            // 
+            this.firstNumberTextBox.Location = new System.Drawing.Point(351, 56);
+            this.firstNumberTextBox.Name = "firstNumberTextBox";
+            this.firstNumberTextBox.Size = new System.Drawing.Size(100, 26);
+            this.firstNumberTextBox.TabIndex = 2;
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Location = new System.Drawing.Point(351, 106);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(100, 26);
+            this.secondNumberTextBox.TabIndex = 3;
+            this.secondNumberTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sumLabel
+            // 
+            this.sumLabel.AutoSize = true;
+            this.sumLabel.Location = new System.Drawing.Point(233, 167);
+            this.sumLabel.Name = "sumLabel";
+            this.sumLabel.Size = new System.Drawing.Size(50, 20);
+            this.sumLabel.TabIndex = 4;
+            this.sumLabel.Text = "Sum :";
+            this.sumLabel.Click += new System.EventHandler(this.sumLabel_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(351, 150);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(84, 54);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate Sum";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.sumLabel);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.firstNumberTextBox);
+            this.Controls.Add(this.secondNumberLabel);
+            this.Controls.Add(this.firstNumberLabel);
+            this.Name = "Form1";
+            this.Text = "Sum Two Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firstNumberLabel;
+        private System.Windows.Forms.Label secondNumberLabel;
+        private System.Windows.Forms.TextBox firstNumberTextBox;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.Label sumLabel;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
