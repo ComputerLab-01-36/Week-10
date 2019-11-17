77088ffbdc558fc075f2629281e98a640ecea80b
Toeyty <61030236@kmitl.ac.th>
2019-11-17 14:41:59 +0700

    commit

---
+++ 61030236/colorDialog/colorDialog/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace colorDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColorDialog_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+                propertyGrid1.Refresh();
+            }
+        }
+    }
+}
