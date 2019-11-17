ce440f4b36ce3e9391c7cd120899917f5397f3c4
Toeyty <61030236@kmitl.ac.th>
2019-11-17 12:43:00 +0700

    commit

--- 61030236/Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.cs
+++ 61030236/Lab10_WindowsFormProperties/Lab10_WindowsFormProperties/Form1.cs
@@ -16,5 +16,15 @@ namespace Lab10_WindowsFormProperties
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
